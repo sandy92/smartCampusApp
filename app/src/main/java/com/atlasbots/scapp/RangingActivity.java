2076cd1c7cf397f8c3416f9cc66525b682355104
Sankeerth <sankee168@gmail.com>
2016-04-11 19:21:56 -0400

    Application reading nearby beacons and rendering pages from play service

--- app/src/main/java/com/atlasbots/scapp/RangingActivity.java
+++ app/src/main/java/com/atlasbots/scapp/RangingActivity.java
@@ -3,10 +3,10 @@ package com.atlasbots.scapp;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
+import android.provider.Settings.Secure;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
@@ -17,6 +17,11 @@ import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.atlasbots.scapp.references.Constants;
 
 /**
  * Created by sank on 4/10/16.
@@ -25,12 +30,16 @@ public class RangingActivity extends ActionBarActivity implements BeaconConsumer
 
     private BeaconManager mBeaconManager;
     Utils utils = new Utils();
+    Map<String, String> availableBeacons = new HashMap<>();
+    private String deviceId = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ranging);
 
+        setContentView(R.layout.activity_ranging);
+        deviceId = Secure.getString(this.getContentResolver(),
+                Secure.ANDROID_ID);
     /*    mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
         // Detect the main Eddystone-UID frame:
         mBeaconManager.getBeaconParsers().add(new BeaconParser().
@@ -42,7 +51,7 @@ public class RangingActivity extends ActionBarActivity implements BeaconConsumer
     @Override
     public void onResume() {
         super.onResume();
-       mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
+        mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
         // Detect the main Eddystone-UID frame:
         mBeaconManager.getBeaconParsers().add(new BeaconParser().
                 setBeaconLayout("s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19"));
@@ -58,34 +67,52 @@ public class RangingActivity extends ActionBarActivity implements BeaconConsumer
             e.printStackTrace();
         }
         mBeaconManager.setRangeNotifier(this);
+
+
     }
+
     @Override
     public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+//        for (Beacon beacon : beacons) {
+//            if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x00) {
+//                // This is a Eddystone-UID frame
+//                Identifier namespaceId = beacon.getId1();
+//                Identifier instanceId = beacon.getId2();
+//                System.out.println("I see a beacon transmitting namespace id: " + namespaceId +
+//                        " and instance id: " + instanceId +
+//                        " approximately " + beacon.getDistance() + " meters away.");
+//            }
+//        }
         for (Beacon beacon : beacons) {
-            if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x00) {
-                // This is a Eddystone-UID frame
-                Identifier namespaceId = beacon.getId1();
-                Identifier instanceId = beacon.getId2();
-                System.out.println("I see a beacon transmitting namespace id: " + namespaceId +
-                        " and instance id: " + instanceId +
-                        " approximately " + beacon.getDistance() + " meters away.");
+            Identifier nameSpaceId = beacon.getId1();
+            Identifier instanceId = beacon.getId2();
+            if (!availableBeacons.keySet().contains(nameSpaceId)) {
+                availableBeacons.put(nameSpaceId.toString(), instanceId.toString());
             }
+            System.out.println(beacon.getId1() + " " + beacon.getId2());
         }
 
-       runOnUiThread(new Runnable() {
-           public void run() {
-               WebView myWebView = (WebView) findViewById(R.id.webview);
-               utils.renderPage("http://10.136.109.45:9000/", myWebView);
-           }
-       });
+        onPause();
+//       runOnUiThread(new Runnable() {
+//           public void run() {
+//               WebView myWebView = (WebView) findViewById(R.id.webview);
+//               utils.renderPage("http://10.136.109.45:9000/", myWebView);
+//           }
+//       });
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        runOnUiThread(new Runnable() {
+            public void run() {
+                WebView myWebView = (WebView) findViewById(R.id.webview);
+                System.out.println("Here is the url : " + Constants.urls.getEventsUrl + generateQueryParams(deviceId, availableBeacons));
+                utils.renderPage(Constants.urls.getEventsUrl + generateQueryParams(deviceId, availableBeacons), myWebView);
+            }
+        });
         mBeaconManager.unbind(this);
     }
-
 
 
     @Override
@@ -102,5 +129,15 @@ public class RangingActivity extends ActionBarActivity implements BeaconConsumer
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public String generateQueryParams(String deviceId, Map<String, String> availableBeacons) {
+        String tmp = "?";
+        Iterator<String> iter = availableBeacons.keySet().iterator();
+        while (iter.hasNext()) {
+            tmp = tmp + Constants.urls.BEACON_ID + iter.next() + "&";
+        }
+        tmp = tmp + Constants.urls.DEVICE_ID + deviceId;
+        return tmp;
     }
 }
---
+++ app/src/main/java/com/atlasbots/scapp/references/Constants.java
@@ -0,0 +1,13 @@
+package com.atlasbots.scapp.references;
+
+/**
+ * Created by sank on 4/11/16.
+ */
+public class Constants {
+
+    static public class urls {
+        public static final String getEventsUrl = "http://10.137.30.56:9000/beacons/";
+        public static final String BEACON_ID = "beaconId=";
+        public static final String DEVICE_ID = "deviceId=";
+    }
+}
