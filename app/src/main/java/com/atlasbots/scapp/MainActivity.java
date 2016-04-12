9e8649dccd32fc592278cb69abf0530814f9792a
Sankeerth <sankee168@gmail.com>
2016-04-11 21:40:23 -0400

    stable code for drawer code

--- app/src/main/java/com/atlasbots/scapp/MainActivity.java
+++ app/src/main/java/com/atlasbots/scapp/MainActivity.java
@@ -8,7 +8,10 @@ import android.content.res.TypedArray;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.RemoteException;
+import android.provider.Settings;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.util.Log;
 import android.view.Menu;
@@ -24,10 +27,27 @@ import com.atlasbots.scapp.adapter.HomeFragment;
 import com.atlasbots.scapp.adapter.NavDrawerListAdapter;
 import com.atlasbots.scapp.models.NavDrawerItem;
 import com.atlasbots.scapp.R;
+import com.atlasbots.scapp.references.Constants;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements BeaconConsumer, RangeNotifier {
+    private BeaconManager mBeaconManager;
+    Utils utils = new Utils();
+    Map<String, String> availableBeacons = new HashMap<>();
+    private String deviceId = "";
 
 //    Utils utils = new Utils();
 //    @Override
@@ -98,6 +118,9 @@ private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        deviceId = Settings.Secure.getString(this.getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+
         setContentView(R.layout.activity_main);
 
         mTitle = mDrawerTitle = getTitle();
@@ -167,11 +190,17 @@ private DrawerLayout mDrawerLayout;
         mDrawerList.setOnItemClickListener(new SlideMenuClickListener());
     }
 
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_ranging, menu);
+    return true;
+}
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -246,7 +275,7 @@ private DrawerLayout mDrawerLayout;
         Fragment fragment = null;
         switch (position) {
             case 0:
-                fragment = new HomeFragment();
+//                fragment = new HomeFragment();
                 break;
             case 1:
 //                fragment = new FindPeopleFragment();
@@ -282,5 +311,80 @@ private DrawerLayout mDrawerLayout;
             // error in creating fragment
             Log.e("MainActivity", "Error in creating fragment");
         }
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
+        // Detect the main Eddystone-UID frame:
+        mBeaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19"));
+        mBeaconManager.bind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        Region region = new Region("all-beacons-region", null, null, null);
+        try {
+            mBeaconManager.startRangingBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        mBeaconManager.setRangeNotifier(this);
+
+
+    }
+
+    @Override
+    public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+//        for (Beacon beacon : beacons) {
+//            if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x00) {
+//                // This is a Eddystone-UID frame
+//                Identifier namespaceId = beacon.getId1();
+//                Identifier instanceId = beacon.getId2();
+//                System.out.println("I see a beacon transmitting namespace id: " + namespaceId +
+//                        " and instance id: " + instanceId +
+//                        " approximately " + beacon.getDistance() + " meters away.");
+//            }
+//        }
+        for (Beacon beacon : beacons) {
+            Identifier nameSpaceId = beacon.getId1();
+            Identifier instanceId = beacon.getId2();
+            if (!availableBeacons.keySet().contains(nameSpaceId)) {
+                availableBeacons.put(nameSpaceId.toString(), instanceId.toString());
+            }
+            System.out.println(beacon.getId1() + " " + beacon.getId2());
+        }
+
+        onPause();
+//       runOnUiThread(new Runnable() {
+//           public void run() {
+//               WebView myWebView = (WebView) findViewById(R.id.webview);
+//               utils.renderPage("http://10.136.109.45:9000/", myWebView);
+//           }
+//       });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        runOnUiThread(new Runnable() {
+            public void run() {
+                WebView myWebView = (WebView) findViewById(R.id.webview);
+                System.out.println("Here is the url : " + Constants.urls.getEventsUrl + generateQueryParams(deviceId, availableBeacons));
+                utils.renderPage(Constants.urls.getEventsUrl + generateQueryParams(deviceId, availableBeacons), myWebView);
+            }
+        });
+        mBeaconManager.unbind(this);
+    }
+
+    public String generateQueryParams(String deviceId, Map<String, String> availableBeacons) {
+        String tmp = "?";
+        Iterator<String> iter = availableBeacons.keySet().iterator();
+        while (iter.hasNext()) {
+            tmp = tmp + Constants.urls.BEACON_ID + iter.next() + "&";
+        }
+        tmp = tmp + Constants.urls.DEVICE_ID + deviceId;
+        return tmp;
     }
 }
