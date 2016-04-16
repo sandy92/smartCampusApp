a649a91899cab30465848de1909737a594f99333
smithamalur <smitha.malur@gmail.com>
2016-04-16 04:31:53 -0400

    Bluetooth check functionality added before scanning for beacons on MainActivity.

--- app/src/main/java/com/atlasbots/scapp/MainActivity.java
+++ app/src/main/java/com/atlasbots/scapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.atlasbots.scapp;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.TypedArray;
@@ -56,13 +58,9 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
     private ListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
 
-    // nav drawer title
     private CharSequence mDrawerTitle;
-
-    // used to store app title
     private CharSequence mTitle;
 
-    // slide menu items
     private String[] navMenuTitles;
     private TypedArray navMenuIcons;
 
@@ -72,19 +70,16 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        BluetoothAdapter m_BluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-//        deviceId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
-        deviceId = m_BluetoothAdapter.getAddress();
         setContentView(R.layout.activity_main);
+        BluetoothAdapter m_BluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        deviceId = m_BluetoothAdapter.getAddress();
+
+
 //        WebView myWebView = (WebView) findViewById(R.id.homewebview);
         //System.out.println("Here is the url : " + Constants.urls.getEventsUrl + generateQueryParams(deviceId, availableBeacons));
 //        utils.renderPage(Constants.urls.getEventsUrl + generateQueryParams(deviceId, availableBeacons), myWebView);
         mTitle = mDrawerTitle = getTitle();
-
-        // load slide menu items
         navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
-
-        // nav drawer icons from resources
         navMenuIcons = getResources()
                 .obtainTypedArray(R.array.nav_drawer_icons);
 
@@ -93,47 +88,30 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
 
         navDrawerItems = new ArrayList<NavDrawerItem>();
 
-        // adding nav drawer items to array
-        // Home
         navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons.getResourceId(0, -1)));
-        // Find People
         navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(1, -1)));
-        // Photos
         navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1)));
-        // Communities, Will add a counter here
         navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3, -1), true, "22"));
-        // Pages
         navDrawerItems.add(new NavDrawerItem(navMenuTitles[4], navMenuIcons.getResourceId(4, -1)));
-        // What's hot, We  will add a counter here
-      //  navDrawerItems.add(new NavDrawerItem(navMenuTitles[5], navMenuIcons.getResourceId(5, -1), true, "50+"));
 
-
-        // Recycle the typed array
         navMenuIcons.recycle();
 
-        // setting the nav drawer list adapter
-        adapter = new NavDrawerListAdapter(getApplicationContext(),
-                navDrawerItems);
+        adapter = new NavDrawerListAdapter(getApplicationContext(), navDrawerItems);
         mDrawerList.setAdapter(adapter);
 
-        // enabling action bar app icon and behaving it as toggle button
         getActionBar().setDisplayHomeAsUpEnabled(false);
         getActionBar().setHomeButtonEnabled(true);
 
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-//                R.drawable.ic_drawer, //nav menu toggle icon
-                R.string.app_name, // nav drawer open - description for accessibility
-                R.string.app_name // nav drawer close - description for accessibility
-        ){
+                R.string.app_name, R.string.app_name
+        ) {
             public void onDrawerClosed(View view) {
                 getActionBar().setTitle(mTitle);
-                // calling onPrepareOptionsMenu() to show action bar icons
                 invalidateOptionsMenu();
             }
 
             public void onDrawerOpened(View drawerView) {
                 getActionBar().setTitle(mDrawerTitle);
-                // calling onPrepareOptionsMenu() to hide action bar icons
                 invalidateOptionsMenu();
             }
         };
@@ -144,7 +122,9 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
             //displayView(0);
         }
         mDrawerList.setOnItemClickListener(new SlideMenuClickListener());
+
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -153,11 +133,9 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // toggle nav drawer on selecting action bar app icon/title
         if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-        // Handle action bar actions click
         switch (item.getItemId()) {
             case R.id.action_settings:
                 return true;
@@ -205,7 +183,7 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
 
     /**
      * Slide menu item click listener
-     * */
+     */
     private class SlideMenuClickListener implements
             ListView.OnItemClickListener {
         @Override
@@ -218,9 +196,8 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
 
     /**
      * Diplaying fragment view for selected nav drawer list item
-     * */
+     */
     private void displayView(int position) {
-        // update the main content by replacing fragments
         Fragment fragment = null;
         switch (position) {
             case 0:
@@ -266,14 +243,17 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
             Log.e("MainActivity", "Error in creating fragment");
         }
     }
+
+
     @Override
     public void onResume() {
         super.onResume();
-        mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
+        verifyBluetooth();
+        /*mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
         // Detect the main Eddystone-UID frame:
         mBeaconManager.getBeaconParsers().add(new BeaconParser().
                 setBeaconLayout("s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19"));
-        mBeaconManager.bind(this);
+        mBeaconManager.bind(this);*/
     }
 
     @Override
@@ -325,5 +305,53 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
         }
         tmp = tmp + Constants.urls.DEVICE_ID + deviceId;
         return tmp;
+    }
+
+    private void verifyBluetooth() {
+
+        try {
+            if (!BeaconManager.getInstanceForApplication(this).checkAvailability()) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Bluetooth not enabled");
+                builder.setMessage("Please enable bluetooth in settings and restart this application.");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        finish();
+                        System.exit(0);
+                    }
+                });
+                builder.show();
+
+            }
+            else{
+
+                mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
+                // Detect the main Eddystone-UID frame:
+                mBeaconManager.getBeaconParsers().add(new BeaconParser().
+                        setBeaconLayout("s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19"));
+                mBeaconManager.bind(this);
+
+            }
+        }
+        catch (RuntimeException e) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Bluetooth LE not available");
+            builder.setMessage("Sorry, this device does not support Bluetooth LE.");
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    finish();
+                    System.exit(0);
+                }
+
+            });
+            builder.show();
+
+        }
+
     }
 }
